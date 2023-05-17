a18892e75c788f3a8b10469ce9e30d403d627f39
hartenstineh <harry.hartenstine@us.af.mil>
2023-05-17 10:02:16 -0400

    set CORP header

--- handle/handle.go
+++ handle/handle.go
@@ -168,6 +168,7 @@ func AddCorsWildcardHeaders(serve http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Cross-Origin-Resource-Policy", "cross-origin")
 		serve(w, r)
 	}
 }
